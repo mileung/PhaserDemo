e939920174806c8f4545268b58fb43434349cf14
Michael Leung <udemyUdemy@Michaels-MacBook-Pro-4.local>
2016-02-13 15:50:12 -0800

    Added Adam sprite and arrow key input

--- state0.js
+++ state0.js
@@ -1,14 +1,32 @@
-var demo = {};
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, adam, speed = 4;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('adam', 'assets/sprites/adam.png');
+    },
     create: function(){
-        game.stage.backgroundColor = '#80FF80';
+        game.stage.backgroundColor = '#800080';
         console.log('state0');
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        adam = game.add.sprite(centerX, centerY, 'adam');
+        adam.anchor.setTo(0.5, 0.5);
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.x += speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.x -= speed;
+        }
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            adam.y -= speed;
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.y += speed;
+        }
+    }
 };
 
 function changeState(i, stateNum){
